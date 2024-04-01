9ed53548737856f54021540f58a3044ff2c49dff
Rafael Souza <rpsouza.dev@gmail.com>
2024-03-31 22:13:11 -0300

    Add register screen layout

---
+++ app/src/main/java/com/rpsouza/taskapp/ui/auth/RegisterFragment.kt
@@ -0,0 +1,27 @@
+package com.rpsouza.taskapp.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rpsouza.taskapp.databinding.FragmentLoginBinding
+import com.rpsouza.taskapp.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
