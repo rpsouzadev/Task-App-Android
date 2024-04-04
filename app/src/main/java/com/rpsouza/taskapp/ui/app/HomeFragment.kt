73f8bc78ecf2a94c9b549e25bfc7524232956161
Rafael Souza <rpsouza.dev@gmail.com>
2024-04-03 23:36:51 -0300

    Add login and logOut with firebase

--- app/src/main/java/com/rpsouza/taskapp/ui/app/HomeFragment.kt
+++ app/src/main/java/com/rpsouza/taskapp/ui/app/HomeFragment.kt
@@ -1,18 +1,18 @@
 package com.rpsouza.taskapp.ui.app
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayoutMediator
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.rpsouza.taskapp.R
 import com.rpsouza.taskapp.databinding.FragmentHomeBinding
-import com.rpsouza.taskapp.databinding.FragmentSplashBinding
 import com.rpsouza.taskapp.ui.adapter.ViewPagerAdapter
+import com.rpsouza.taskapp.utils.showBottomSheet
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -31,6 +31,21 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initTabs()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnLogOut.setOnClickListener {
+            showBottomSheet(
+                titleButton = R.string.text_button_dialog_confirm_log_out,
+                titleDialog = R.string.text_title_dialog_confirm_log_out,
+                message = getString(R.string.text_message_dialog_confirm_log_out),
+                onClick = {
+                    Firebase.auth.signOut()
+                    findNavController().navigate(R.id.action_homeFragment_to_authentication)
+                }
+            )
+        }
     }
 
     private fun initTabs() {
@@ -47,7 +62,6 @@ class HomeFragment : Fragment() {
             tab.text = getString(pageAdapter.getTitle(position))
         }.attach()
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/rpsouza/taskapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/rpsouza/taskapp/ui/auth/LoginFragment.kt
@@ -1,20 +1,25 @@
 package com.rpsouza.taskapp.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.rpsouza.taskapp.R
 import com.rpsouza.taskapp.databinding.FragmentLoginBinding
-import com.rpsouza.taskapp.databinding.FragmentSplashBinding
 import com.rpsouza.taskapp.utils.showBottomSheet
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +31,8 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
 
         initListeners()
     }
@@ -52,14 +59,31 @@ class LoginFragment : Fragment() {
 
         if (email.isNotEmpty()) {
             if (password.isNotEmpty()) {
-                findNavController().navigate(R.id.action_global_homeFragment)
+                binding.progressBar.isVisible = true
+                handleLogin(email, password)
             } else {
                 showBottomSheet(message = getString(R.string.password_empty))
             }
-
         } else {
             showBottomSheet(message = getString(R.string.email_empty))
         }
+    }
+
+    private fun handleLogin(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_global_homeFragment)
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        task.exception?.message,
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                    binding.progressBar.isVisible = false
+                }
+            }
     }
 
     override fun onDestroyView() {
