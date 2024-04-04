8a08d186cf41628209fd0cf7580491cbd393e32b
Rafael Souza <rpsouza.dev@gmail.com>
2024-04-03 22:49:27 -0300

    Add checkif the user is already authenticated

--- app/src/main/java/com/rpsouza/taskapp/ui/SplashFragment.kt
+++ app/src/main/java/com/rpsouza/taskapp/ui/SplashFragment.kt
@@ -1,19 +1,21 @@
 package com.rpsouza.taskapp.ui
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.rpsouza.taskapp.R
 import com.rpsouza.taskapp.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,11 +29,18 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 3000)
+        auth = Firebase.auth
+
+        checkAuth()
     }
 
     private fun checkAuth() {
-        findNavController().navigate(R.id.action_splashFragment_to_authentication)
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        } else {
+            findNavController().navigate(R.id.action_splashFragment_to_authentication)
+        }
     }
 
     override fun onDestroyView() {
