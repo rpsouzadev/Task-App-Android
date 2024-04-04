272bc35273d799d959cc55af7c6ab4175d16bcdc
Rafael Souza <rpsouza.dev@gmail.com>
2024-04-04 00:11:19 -0300

    Add recover account user with firebase

--- app/src/main/java/com/rpsouza/taskapp/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/rpsouza/taskapp/ui/auth/RecoverAccountFragment.kt
@@ -1,21 +1,25 @@
 package com.rpsouza.taskapp.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.navigation.fragment.findNavController
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.rpsouza.taskapp.R
 import com.rpsouza.taskapp.databinding.FragmentRecoverAccountBinding
-import com.rpsouza.taskapp.databinding.FragmentRegisterBinding
 import com.rpsouza.taskapp.utils.initToolbar
 import com.rpsouza.taskapp.utils.showBottomSheet
 
 class RecoverAccountFragment : Fragment() {
     private var _binding: FragmentRecoverAccountBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,6 +31,8 @@ class RecoverAccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
 
         initToolbar(binding.toolbar)
         initListeners()
@@ -42,13 +48,28 @@ class RecoverAccountFragment : Fragment() {
         val email = binding.editEmail.text.toString().trim()
 
         if (email.isNotEmpty()) {
-            Toast.makeText(
-                requireContext(), "Tudo certo.", Toast.LENGTH_SHORT
-            ).show()
-
+            binding.progressBar.isVisible = true
+            handleRecoverAccountUser(email)
         } else {
             showBottomSheet(message = getString(R.string.email_empty))
         }
+    }
+
+    private fun handleRecoverAccountUser(email: String) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                binding.progressBar.isVisible = false
+
+                if (task.isSuccessful) {
+                    showBottomSheet(message = getString(R.string.text_message_recover_account))
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        task.exception?.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 
     override fun onDestroyView() {
